c4596b340ea0bc0973803cf8eaa39bf821c43539
mgackowski <mwgackowski@gmail.com>
2023-01-07 18:35:23 +0000

    Experimenting with TMP shadows

---
+++ Assets/LineViewNonCanvas.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LineViewNonCanvas : Yarn.Unity.LineView
+{
+    public TextMeshPro lineText = null;
+}
