49445950bc17267133887d097014b5a24e199a7a
mgackowski <mwgackowski@gmail.com>
2023-03-28 21:30:48 +0100

    Door animations and script

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+using Yarn.Unity;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    [YarnCommand("open")]
+    public void Open()
+    {
+        anim.SetTrigger("Open");
+    }
+
+    [YarnCommand("close")]
+    public void Close()
+    {
+        anim.SetTrigger("Close");
+    }
+
+    [YarnCommand("forced")]
+    public void SetForced(bool value)
+    {
+        anim.SetBool("Forced", value);
+    }
+}
