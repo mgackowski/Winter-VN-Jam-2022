1ebd1b60cd72a5e11413ffa7e91fe3106c4f328f
mgackowski <mwgackowski@gmail.com>
2023-05-28 20:13:40 +0100

    Main Menu using existing options view

--- Assets/Scripts/SceneInfo.cs
+++ Assets/Scripts/SceneInfo.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using Yarn.Unity;
 
 public static class SceneInfo
 {
@@ -23,6 +24,44 @@ public static class SceneInfo
     public static void DeregisterCamera(CinemachineVirtualCamera camera)
     {
         cameras.Remove(camera);
+    }
+
+    [YarnCommand("setQuality")]
+    public static void SetQuality(string quality)
+    {
+        int balancedPreset = 0;
+        int highPreset = 1;
+        
+        string[] names = QualitySettings.names;
+        for(int i = 0; i < names.Length; i++)
+        {
+            if(names[i].Equals("Balanced"))
+            {
+                balancedPreset = i;
+            }
+            else if(names[i].Equals("High"))
+            {
+                highPreset = i;
+            }
+        }
+
+        switch (quality.ToLower())
+        {
+            case "balanced":
+                QualitySettings.SetQualityLevel(balancedPreset);
+                break;
+            case "high":
+                QualitySettings.SetQualityLevel(highPreset);
+                break;
+            default:
+                Debug.LogErrorFormat("Couldn't find quality preset {0}, check if the Yarn script calls the right parameter?", quality);
+                break;
+        }
+    }
+
+    public enum QualityLevel
+    {
+        High, Balanced
     }
 
 }
